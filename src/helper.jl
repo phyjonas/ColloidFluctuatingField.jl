c33a7214ab46dfe80de5f2e6716d38d027dcf7bb
Jonas J <jonas.jager@hotmail.de>
2022-02-04 23:31:03 +0000

    fixed Gaussian pot

--- src/helper.jl
+++ src/helper.jl
@@ -66,14 +66,14 @@ function V_gaussian(V::Array{<:Array{Float64}}, x::Array, t::Float64, xarray::Ar
     R = CartesianIndices(V[1])
     d = length(size(V[1]))
     for I in R
-        V[1][I] = exp(-sum((x[i] % L - xarray[I[i]])^2 / re^2 for i = 1:d)) / (re)^(d)
+        V[1][I] = exp(-sum((x[i] % L - xarray[I[i]])^2 / (2*re^2) for i = 1:d)) / (re)^(d)
         for j = 2:length(V)
             V[j][I] =
-                -2 * (xarray[I[j-1]] - x[j-1] % L) / re^(2+d) *
-                exp(-sum((x[i] % L - xarray[I[i]])^2 / re for i = 1:d))
+                - (xarray[I[j-1]] - x[j-1] % L) / re^(2+d) *
+                exp(-sum((x[i] % L - xarray[I[i]])^2 / (2*re^2) for i = 1:d))
         end
     end
-    return 1 / sqrt(pi) * V
+    return 1 / sqrt(pi)^d * V
 end
 
 function V_fct_pinned(V, x, t, xarray, re)
